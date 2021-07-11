d85abb564085b071581278580866c784d0455e5d
Ernestas <ernestas.araminas@ktu.edu>
2021-07-11 03:07:17 +0300

    New functionallity: now you can upload file with ibans

--- IBAN_Checking_Web/Pages/Index.cshtml.cs
+++ IBAN_Checking_Web/Pages/Index.cshtml.cs
@@ -1,9 +1,12 @@
 ﻿using IBAN_Checking_Library;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,6 +14,39 @@ namespace IBAN_Checking_Web.Pages
 {
     public class IndexModel : PageModel
     {
+        private readonly ILogger<IndexModel> _logger;
+        private readonly IHostEnvironment _environment;
+
+        [BindProperty]
+        public IFormFile UploadedFile { get; set; }
+
+        public async Task OnPostAsync()
+        {
+            if (UploadedFile == null || UploadedFile.Length == 0)
+            {
+                return;
+            }
+
+            _logger.LogInformation($"Uploading {UploadedFile.FileName}.");
+            string targetFileName = $"{_environment.ContentRootPath}/{UploadedFile.FileName}";
+
+            using (var stream = new FileStream(targetFileName, FileMode.Create))
+            {
+                await UploadedFile.CopyToAsync(stream);
+
+            }
+
+            using (var sr = new StreamReader(targetFileName))
+            {
+                Input = sr.ReadToEnd();
+                OnPostResult();
+                sr.Close();
+            }
+
+            FileInfo fi = new FileInfo(targetFileName);
+            fi.Delete();
+        }
+
         [BindProperty]
         public string Input { get; set; }
 
@@ -45,9 +81,12 @@ namespace IBAN_Checking_Web.Pages
             Checker.Result = "";
         }
 
-        public IndexModel(IChecker checker)
+
+        public IndexModel(IChecker checker, ILogger<IndexModel> logger, IHostEnvironment environment)
         {
             Checker = checker;
+            _logger = logger;
+            _environment = environment;
         }
     }
 }
